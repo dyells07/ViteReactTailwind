923e1e8dc7df6ee0ba34986684c4dca8aeedd80d
dyells07 <bhimkhanal999@gmail.com>
2023-09-28 23:39:29 +0545

    Context Banauna Sikeko

--- ContextConcepts/src/App.jsx
+++ ContextConcepts/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
+
 import './App.css'
+import Login from './components/login'
+import Profile from './components/profile'
+import UserContextProvider from './context/UserContextProvider'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-     
-    </>
+    <UserContextProvider>
+      <h1>Which Folder is Important ? </h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
 
 export default App
+
---
+++ ContextConcepts/src/context/UserContext.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserContext = React.createContext()
+
+export default UserContext;
+
+
+//Context is Global Variable if we create COntext we should have provide Provider
+{/* <UserContext>
+
+</UserContext> */}

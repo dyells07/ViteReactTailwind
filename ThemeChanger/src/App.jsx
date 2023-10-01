6975785f5604742d183936cc87c26c07c5cd978d
dyells07 <bhimkhanal999@gmail.com>
2023-10-01 21:56:09 +0545

    switch off huna lagyo laptop

--- ThemeChanger/src/App.jsx
+++ ThemeChanger/src/App.jsx
@@ -1,15 +1,45 @@
-import { useState } from 'react'
-
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import { ThemeProvider } from './contexts/theme';
+import ThemeBtn from './components/ThemeBtn';
+import Card from './components/Card';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light");
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("themeMode");
+    if (storedTheme) {
+      setThemeMode(storedTheme);
+    }
+  }, []);
+  
+  useEffect(() => {
+    localStorage.setItem("themeMode", themeMode);
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
 
   return (
-    <>
-    
-    </>
-  )
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+          <Card />
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- ThemeChanger/src/components/Card.jsx
+++ ThemeChanger/src/components/Card.jsx
@@ -1,77 +1,65 @@
 import React from 'react'
 
 export default function Card() {
+    const products = [
+        {
+          id: 1,
+          name: 'Osmocote Fertilizer',
+          href: '#',
+          price: 'रु 100',
+          imageSrc: './small.jpg',
+          imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+        },
+        {
+          id: 2,
+          name: 'Vermicompost Ferilizer',
+          href: '#',
+          price: 'रु 50',
+          imageSrc: './chemical.jpg',
+          imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
+        },
+        {
+          id: 3,
+          name: 'Tomato fertilizer',
+          href: '#',
+          price: 'रु 189',
+          imageSrc: './from.jpg',
+          imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+        },
+        {
+          id: 4,
+          name: 'Oragnic',
+          href: '#',
+          price: 'रु 200',
+          imageSrc: './hand.jpg',
+          imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+        },
+        
+      ]
+      
     return (
-        <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
-            </a>
-            <div className="px-5 pb-5">
-                <a href="/">
-                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-                    </h5>
-                </a>
-                <div className="flex items-center mt-2.5 mb-5">
-                    <svg
-                        className="w-4 h-4 text-yellow-300 mr-1"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        viewBox="0 0 22 20"
-                    >
-                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                        className="w-4 h-4 text-yellow-300 mr-1"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        viewBox="0 0 22 20"
-                    >
-                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                        className="w-4 h-4 text-yellow-300 mr-1"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        viewBox="0 0 22 20"
-                    >
-                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                        className="w-4 h-4 text-yellow-300 mr-1"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        viewBox="0 0 22 20"
-                    >
-                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                        className="w-4 h-4 text-gray-200 dark:text-gray-600"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        viewBox="0 0 22 20"
-                    >
-                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
-                        4.0
-                    </span>
-                </div>
-                <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-                    <a
-                        href="/"
-                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                    >
-                        Add to cart
-                    </a>
-                </div>
-            </div>
+        <div className="bg-white">
+            
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <h2 className="sr-only">Products</h2>
+  
+          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            {products.map((product) => (
+              <a key={product.id} href={product.href} className="group">
+            <div className="w-full aspect-w-1 aspect-h-1 overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+    <img
+        src={product.imageSrc}
+        alt={product.imageAlt}
+        className="object-cover object-center group-hover:opacity-75 aspect-w-1 aspect-h-1"
+    />
+</div>
+<h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+<p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+
+              </a>
+            ))}
+          </div>
         </div>
+      </div>
     );
 }

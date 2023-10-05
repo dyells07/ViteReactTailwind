43d129109d7e017ec624ca6be7b1acf338c07491
dyells07 <bhimkhanal999@gmail.com>
2023-10-05 21:16:46 +0545

    Started

--- 001FullStackReactWithDotnet/ReactPart/reactanddotnet/src/Variables.js
+++
@@ -1,4 +0,0 @@
-export const variables={
-    API_URL:"http://localhost:49146/api/",
-    PHOTO_URL:"http://localhost:49146/Photos/"
-}
---
+++ 001RESTfulDotnetReact/API/backend/backend/Program.cs
@@ -0,0 +1,39 @@
+using backend.Core.AutoMapperConfig;
+using backend.Core.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+// DB Configuration
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// Automapper Configuration
+builder.Services.AddAutoMapper(typeof(AutoMapperConfigProfile));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
